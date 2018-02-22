6fafba21af54da60da176f10161986ffff9e1796
peekinternational <peekinternational@gmail.com>
2018-02-22 17:33:16 +0500

    nabeel change

--- resources/views/frontend/view-job-detail.blade.php
+++ resources/views/frontend/view-job-detail.blade.php
@@ -20,7 +20,7 @@ if($job->companyLogo != ''){
 			<span style="text-transform: uppercase;font-size: 26px;">{{$job->companyName}}</span>
                 <p style="font-size: 18px;margin-top: 24px; margin-left: 6px;">{{ $job->title }},  &nbsp;<span style="font-size: 13px; padding-top: 9px;">{{ JobCallMe::cityName($job->city) }}, {{ JobCallMe::countryName($job->country) }} </span> &nbsp;<span class="label" style="background-color: {{ $colorArr[array_rand($colorArr)] }}">
 				{{ $job->p_title }}
-				</span><span style="font-size:9px;margin-left:100px">Never pay for job application, test or interview. <a href="{{ url('/safety')}}">more</a></span></p>
+				</span><span style="font-size:9px;margin-left:13px">Never pay for job application, test or interview. <a href="{{ url('/safety')}}">more</a></span></p>
 				
 				</div>
 				
