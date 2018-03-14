dac97d57c628687e236c128d819064f5b0061b4a
saadahmed <saad.ahmed@rockvillegroup.com>
2018-03-14 17:47:36 +0500

    imp

--- app/User.php
+++ app/User.php
@@ -8,16 +8,19 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-protected $primaryKey = 'userId';
+    protected $primaryKey = 'userId';
+    protected $table = 'jcm_users';
+    const CREATED_AT = 'createdTime';
+    const UPDATED_AT = 'modifiedTime';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'userId','secretId','companyId','firstName','lastName','email','username','password','phoneNumber','type','	status','country','city','state','remember_token','profilePhoto','about','createdTime','modifiedTime','updated_at',
+        'userId','secretId','companyId','firstName','lastName','email','username','password','phoneNumber','type','	status','country','city','state','addby','remember_token','profilePhoto','profileImage','about','subscribe','fbId','user_status','createdTime','modifiedTime',
     ];
-protected $table = 'jcm_users';
+
     /**
      * The attributes that should be hidden for arrays.
      *
