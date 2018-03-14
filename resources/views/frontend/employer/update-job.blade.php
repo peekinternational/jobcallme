d251182cd9dddbc6010985e3996b937e127dfd10
peekinternational <peekinternational@gmail.com>
2018-03-14 16:42:09 +0500

    sajid update code

--- resources/views/frontend/employer/update-job.blade.php
+++ resources/views/frontend/employer/update-job.blade.php
@@ -473,13 +473,7 @@
         border-color: #4d90fe;
       }
 
-      #title {
-        color: #fff;
-        background-color: #4d90fe;
-        font-size: 25px;
-        font-weight: 500;
-        padding: 6px 12px;
-      }
+      
 </style>
 </section>
 @endsection
