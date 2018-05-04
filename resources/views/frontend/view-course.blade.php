62afa5c7208a4147ab2367c3451ab07700bc5ddc
peekinternational <peekinternational@gmail.com>
2018-05-04 17:00:18 +0500

    custom sas

--- resources/views/frontend/view-course.blade.php
+++ resources/views/frontend/view-course.blade.php
@@ -244,8 +244,10 @@
                                         <div class="col-md-9 col-xs-9">
                                             <div class="form-group">
                                                 <textarea name="comment" id="comment" class="form-control" rows="3"></textarea>
-                                            </div>    
+                                            </div> 
+                                            <p style="color: #999999;">if you need more discus you may use jobcallme Live video and chat which you can use multi video and chat</p>   
                                         </div>
+
                                         <div class="col-md-2 col-xs-offset-8" style="padding-top: 15px;"><button class="btn btn-success" id="comment-btn">Submit</button></div>
                                     </div>    
                                 </div>
